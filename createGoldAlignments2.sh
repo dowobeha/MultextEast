8763b1aee019c930f6d4ea446a44cbb0b36fe804
Lane Schwartz <dowobeha@gmail.com>
2016-02-25 09:38:29 -0600

    Updated script

--- createGoldAlignments2.sh
+++ createGoldAlignments2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IN_FILE=$1
-OUT_FILE=$1.parallel2
 LANGs=$2
+IN_FILE=$1
+OUT_FILE=$1_$(echo "${LANGs}" | tr -d ' ').parallel2
 
 IN_DIR=multextEast
 OUT_DIR=multextEastAligned
@@ -25,4 +25,22 @@ diff -y <(cat ${OUT_FILE}.gold | tr -d "<" | tr -d ">") <(cat ${OUT_FILE}.actual
 
 clear
 
-grep "[<>|]" ${OUT_FILE}.diff
+#grep "[<>|]" ${OUT_FILE}.diff
+
+TRUE_POSITIVES=$(cat ${OUT_FILE}.diff | grep -v "SPEAKER" | grep -v "[<>|]" | wc -l)
+
+#FALSE_POSITIVES=$(grep "[>|]" ${OUT_FILE}.diff | wc -l)
+#FALSE_NEGATIVES=$(grep "[<|]" ${OUT_FILE}.diff | wc -l)
+
+ALL_POSITIVES=$(grep -c -v "^<" ${OUT_FILE}.actual)
+ALL_RELEVANT=$(grep -c -v "<" ${OUT_FILE}.gold)
+
+PRECISION=$(ruby -e "puts sprintf( \"%0.04f\", ((${TRUE_POSITIVES}).to_f / ${ALL_POSITIVES}))")
+RECALL=$(ruby -e "puts sprintf( \"%0.04f\", ((${TRUE_POSITIVES}).to_f / ${ALL_RELEVANT}))")
+
+F_MEASURE=$(ruby -e "puts sprintf( \"%0.04f\", (2 * (${PRECISION} * ${RECALL}) / (${PRECISION} + ${RECALL})))")
+
+echo -e "${LANGs}\tprecision=${PRECISION}\t${TRUE_POSITIVES}/${ALL_POSITIVES}"                           > ${OUT_FILE}.stats
+echo -e "${LANGs}\t   recall=${RECALL}\t${TRUE_POSITIVES}/${ALL_RELEVANT}"                              >> ${OUT_FILE}.stats
+echo -e "${LANGs}\tf-measure=${F_MEASURE}\t2 * (${PRECISION} * ${RECALL}) / (${PRECISION} + ${RECALL})" >> ${OUT_FILE}.stats
+
