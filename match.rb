72c073b2889b2515ee54c8ca0429522c0966dd4c
Lane Schwartz <dowobeha@gmail.com>
2016-02-29 14:23:00 -0600

    Seems to be working better

--- match.rb
+++ match.rb
@@ -33,6 +33,9 @@ if (file_names.size < 2)
 	exit(1)
 end
 
+languages=Set.new
+
+
 all_ids=Hash.new{|k,v| k[v]=Set.new}
 
 file_names.each{|alignment_file|
@@ -41,6 +44,8 @@ file_names.each{|alignment_file|
 		line.gsub(/"/, " ").split.each{ |token|
 			if isID(token)
 				ids.add(token)
+				file,rest = split(token)
+				languages.add(file)
 			end
 		}
 		ids.each{|id1|
@@ -59,25 +64,73 @@ def still_merging(id_map)
 	return false
 end
 
-while (still_merging(visited))
+def recursivelyCalculateUnion(id1, id_map, visited_map, results_set)
+	#puts "\n\nrecursivelyCalculateUnion for #{id1}"
+	id_map[id1].each{|id2|
+		#print "Adding #{id2} to results_set\t#{results_set.to_a.to_s}"
+		results_set.add(id2)
+		#puts "\t#{results_set.to_a.to_s}"
+	}
+	visited_map[id1]=true
+	id_map[id1].each{|id2|
+		unless visited_map[id2] == true
+			recursivelyCalculateUnion(id2, id_map, visited_map, results_set)
+		end
+	}
+	return results_set
+end
+
+vis=Hash.new{|k,v| k[v]=false}
+all_ids.each{|id1,set|
+	unless vis[id1]==true
+		union = recursivelyCalculateUnion(id1, all_ids, vis, Set.new)
+		union.each{|id2|
+			all_ids[id2]=union
+		}
+	end	
+}
+
+puts "<?xml version=\"1.0\" encoding=\"us-ascii\"?>"
+puts "<linkGrp xmlns=\"http://www.tei-c.org/ns/1.0\" type=\"alignment\" corresp=\"#{languages.to_a.sort.join(" ")}\">"
+vis2=Hash.new{|k,v| k[v]=false}
+all_ids.each{|id1,set|
+	unless vis2[id1]==true
+		alignment=Hash.new{|k,v| k[v]=0}
+		all_ids[id1].to_a.each{|id|
+			file,value=split(id)
+			alignment[file] += 1
+			#puts "alignment[#{file}] = #{alignment[file]}"
+		}
+		print "<link n=\""
+		print languages.to_a.sort.map{|file_name| alignment[file_name]}.join(":")
+		puts "\" targets=\"#{all_ids[id1].to_a.sort.join(" ")}\">"
+		set.each{|id2| vis2[id2]=true }
+	end	
+}
+puts "</linkGrp>"
+
+
+if false
+	while (still_merging(visited))
+		all_ids.sort.each{|id1,set|
+			unless visited[id1]
+				#puts "#{id1}\t<link targets=\"#{all_ids[id1].to_a.sort.join(" ")}\">"
+				set.each{|id2|
+					unless all_ids[id2].include?(id1)
+						all_ids[id2].add(id1)
+						visited[id1]=false
+					end
+				}
+				visited[id1]=true
+			end
+		}
+	end
+
+	visited2=Hash.new{|k,v| k[v]=false}
 	all_ids.sort.each{|id1,set|
-		unless visited[id1]
-			#puts "#{id1}\t<link targets=\"#{all_ids[id1].to_a.sort.join(" ")}\">"
-			set.each{|id2|
-				unless all_ids[id2].include?(id1)
-					all_ids[id2].add(id1)
-					visited[id1]=false
-				end
-			}
-			visited[id1]=true
+		unless visited2[id1]
+			puts "#{id1}\t<link targets=\"#{all_ids[id1].to_a.sort.join(" ")}\">"
+			set.each{|id2| visited2[id2]=true }
 		end
 	}
 end
-
-visited2=Hash.new{|k,v| k[v]=false}
-all_ids.sort.each{|id1,set|
-	unless visited2[id1]
-		puts "#{id1}\t<link targets=\"#{all_ids[id1].to_a.sort.join(" ")}\">"
-		set.each{|id2| visited2[id2]=true }
-	end
-}
